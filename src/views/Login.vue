17ad91949b4f9360bbccc97cc1d9f0a88550c5d0
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-10-06 11:25:57 +0200

    Added Vuex

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,31 @@
 <template>
   <div id="login">
-    <h1>This is login page</h1>
+    <v-form>
+      <v-text-field
+        name="username"
+        label="Username"
+        id="username"
+      ></v-text-field>
+
+      <v-text-field
+        name="password"
+        label="Password"
+        id="password"
+        type="password"
+      ></v-text-field>
+
+      <v-btn color="success" id="submit">Submit</v-btn>
+    </v-form>
   </div>
 </template>
+
+<script>
+export default {
+  methods: () => {
+    const { username, password } = this;
+    this.$store.dispatch(AUTH_REQUEST, { username, password }).then(() => {
+      this.$router.push("/");
+    });
+  },
+};
+</script>
