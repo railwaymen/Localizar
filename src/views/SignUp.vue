741e681e023cf8c9c4c5a701a114da9f351daf5f
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-10-09 14:44:19 +0200

    Added frontend

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,5 +1,47 @@
 <template>
-  <div id="sign_up">
-    <h1>This is a Sign Up Page</h1>
+  <div id="login">
+    <v-form>
+      <v-text-field
+        v-model="username"
+        label="Username"
+        id="username"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="password"
+        label="Password"
+        id="password"
+        type="password"
+      ></v-text-field>
+
+      <v-btn color="success" id="submit" @click="signup">Sign up</v-btn>
+    </v-form>
   </div>
 </template>
+
+<script>
+import { apiClient } from "../modules/apiClient";
+
+export default {
+  data: () => ({
+    username: "",
+    password: "",
+  }),
+  methods: {
+    signup() {
+      const username = this.username;
+      const password = this.password;
+      apiClient
+        .post("/users", { username, password })
+        .then(() => {
+          this.$store.dispatch("AUTH_REQUEST").then(() => {
+            this.$router.push("/login");
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
