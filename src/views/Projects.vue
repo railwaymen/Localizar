ef731fa828b1241b4ac2cbf5783af48e10ab7fc9
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-10-14 12:25:43 +0200

    View for the list of projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,8 +1,47 @@
 <template>
   <div id="projects">
-    <h1>{{ $t("projects.title") }}</h1>
-    <v-btn :to="{ name: 'create_project' }">
-      {{ $t("projects.button.create_new") }}
-    </v-btn>
+    <v-container>
+      <v-row justify="center">
+        <v-col md="8">
+          <div class="form-container">
+            <v-row>
+              <h1>{{ $t("projects.title") }}</h1>
+              <v-spacer></v-spacer>
+              <v-btn :to="{ name: 'create_project' }">
+                {{ $t("projects.button.create_new") }}
+              </v-btn>
+            </v-row>
+
+            <v-data-table
+              :headers="headers"
+              :items="projects"
+              @click:row="rowClicked"
+            >
+            </v-data-table>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
+
+<script>
+import i18n from "@/i18n";
+
+export default {
+  data: () => ({
+    headers: [
+      { text: i18n.t("projects.table_headers.project_name"), value: "name" },
+    ],
+    projects: [{ name: "Yes", slug: "yes" }],
+  }),
+  methods: {
+    rowClicked(project) {
+      this.$router.push({
+        name: "project_panel",
+        params: { slug: project.slug },
+      });
+    },
+  },
+};
+</script>
