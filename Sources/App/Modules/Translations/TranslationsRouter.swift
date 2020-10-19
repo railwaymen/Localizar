5ff3dba1826bfe99ea9262d58dd0dd777c821179
Bartłomiej Świerad <bartlomiej.swierad@railwaymen.org>
2020-10-19 12:04:34 +0200

    Added endpoint for getting project details

--- Sources/App/Modules/Translations/TranslationsRouter.swift
+++ Sources/App/Modules/Translations/TranslationsRouter.swift
@@ -4,10 +4,10 @@ struct TranslationsRouter: RouteCollection {
     private let controller: TranslationsController = .init()
     
     func boot(routes: RoutesBuilder) throws {
-        let protectedRoutes = routes
+        let routesV1 = routes
             .grouped("v1")
             .grouped(UserTokenAuthenticator<UserTokenModel>())
             .grouped(UserModel.guardMiddleware())
-        protectedRoutes.get("projects", .parameter(ProjectModel.slugParameter), "translations", use: controller.getWithPagination)
+        routesV1.get("projects", .parameter(ProjectModel.slugParameter), "translations", use: controller.getWithPagination)
     }
 }
